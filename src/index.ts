4c7ca5d88e58a831b074e33597632bf0a5845bcd
Dean Fox <deanfoxcd@gmail.com>
2025-01-22 19:55:31 -0500

    initial commit + setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/// <reference types="@types/google.maps" />
+
+// import { Company } from './Company';
+// import { User } from './User';
+
+// const user = new User();
+// console.log(user);
+
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
